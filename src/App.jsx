545b579b17cab3a62d81de3aeb1aee392f604e99
Zahid de Jesus Avalos Hernandez <l19350283@tuctepec.tecnm.mx>
2023-03-03 10:04:50 -0600

    diseñamos la calculadora con sus botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,45 @@
+import './App.css'
+
 function App() {
   return (
     <div>
       <h1>Calculator</h1>
-      <table  style={{width: '400px'}}>
+      <table>
+        {/* First now*/}
         <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td colSpan={"4"}>RESULT</td>
         </tr>
+        {/* Second now*/}
         <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td><button type="button" className="btn btn-primary">C</button></td>
+        <td><button type="button" className="btn btn-primary">/</button></td>
+        <td><button type="button" className="btn btn-primary">*</button></td>
+        <td><button type="button" className="btn btn-primary">-</button></td>
         </tr>
+        {/* Third now*/}
         <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td><button type="button" className="btn btn-primary">7</button></td>
+        <td><button type="button" className="btn btn-primary">8</button></td>
+        <td><button type="button" className="btn btn-primary">9</button></td>
+        <td rowSpan={"2"}><button type="button" className="btn btn-primary">+</button></td>
         </tr>
+        {/* Fourth now*/}
         <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td><button type="button" className="btn btn-primary">4</button></td>
+        <td><button type="button" className="btn btn-primary">5</button></td>
+        <td><button type="button" className="btn btn-primary">6</button></td>
         </tr>
+        {/* Fifth now*/}
         <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td><button type="button" className="btn btn-primary">1</button></td>
+        <td><button type="button" className="btn btn-primary">2</button></td>
+        <td><button type="button" className="btn btn-primary">3</button></td>
+        <td rowSpan={"2"}><button type="button" className="btn btn-primary">=</button></td>
+        </tr>
+        {/* Six now*/}
+        <tr>
+        <td colSpan={"2"}><button type="button" className="btn btn-primary">0</button></td>
+        <td><button type="button" className="btn btn-primary">.</button></td>
         </tr>
       </table>
     </div>
