02399e38f3d940b37d6f5f083c73ced32b538611
Zahid de Jesus Avalos Hernandez <l19350283@tuctepec.tecnm.mx>
2023-03-07 10:02:57 -0600

    alto y ancho de botones

--- src/App.jsx
+++ src/App.jsx
@@ -7,39 +7,39 @@ function App() {
       <table>
         {/* First now*/}
         <tr>
-        <td colSpan={"4"}>RESULT</td>
+          <td colSpan={"4"}>RESULT</td>
         </tr>
         {/* Second now*/}
         <tr>
-        <td><button type="button" className="btn btn-primary">C</button></td>
-        <td><button type="button" className="btn btn-primary">/</button></td>
-        <td><button type="button" className="btn btn-primary">*</button></td>
-        <td><button type="button" className="btn btn-primary">-</button></td>
+          <td><button type="button" className="btn btn-primary w-75">C</button></td>
+          <td class="position-relative"><button type="button" className="btn btn-primary w-75">/</button></td>
+          <td><button type="button" className="btn btn-primary w-75">*</button></td>
+          <td><button type="button" className="btn btn-primary w-75">-</button></td>
         </tr>
         {/* Third now*/}
         <tr>
-        <td><button type="button" className="btn btn-primary">7</button></td>
-        <td><button type="button" className="btn btn-primary">8</button></td>
-        <td><button type="button" className="btn btn-primary">9</button></td>
-        <td rowSpan={"2"}><button type="button" className="btn btn-primary">+</button></td>
+          <td><button type="button" className="btn btn-primary w-75">7</button></td>
+          <td><button type="button" className="btn btn-primary w-75">8</button></td>
+          <td><button type="button" className="btn btn-primary w-75">9</button></td>
+          <td rowSpan={"2"}><button type="button" className="btn btn-primary w-75" style={{height: "80px"}}>+</button></td>
         </tr>
         {/* Fourth now*/}
         <tr>
-        <td><button type="button" className="btn btn-primary">4</button></td>
-        <td><button type="button" className="btn btn-primary">5</button></td>
-        <td><button type="button" className="btn btn-primary">6</button></td>
+          <td><button type="button" className="btn btn-primary w-75">4</button></td>
+          <td><button type="button" className="btn btn-primary w-75">5</button></td>
+          <td><button type="button" className="btn btn-primary w-75">6</button></td>
         </tr>
         {/* Fifth now*/}
         <tr>
-        <td><button type="button" className="btn btn-primary">1</button></td>
-        <td><button type="button" className="btn btn-primary">2</button></td>
-        <td><button type="button" className="btn btn-primary">3</button></td>
-        <td rowSpan={"2"}><button type="button" className="btn btn-primary">=</button></td>
+          <td><button type="button" className="btn btn-primary w-75">1</button></td>
+          <td><button type="button" className="btn btn-primary w-75">2</button></td>
+          <td><button type="button" className="btn btn-primary w-75">3</button></td>
+          <td rowSpan={"2"}><button type="button" className="btn btn-primary w-75" style={{height: "80px"}}>=</button></td>
         </tr>
         {/* Six now*/}
         <tr>
-        <td colSpan={"2"}><button type="button" className="btn btn-primary">0</button></td>
-        <td><button type="button" className="btn btn-primary">.</button></td>
+          <td colSpan={"2"}><button type="button" className="btn btn-primary" style={{width: "175px"}}>0</button></td>
+          <td><button type="button" className="btn btn-primary w-75">.</button></td>
         </tr>
       </table>
     </div>
